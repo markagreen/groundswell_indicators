dc38f9c42005727761bcce1488a95be107eefa26
Mark A Green <mark.green@liverpool.ac.uk>
2024-07-11 12:23:17 +0100

    Update preprocessing.py

--- accessibility/ukroutes/preprocessing.py
+++ accessibility/ukroutes/preprocessing.py
@@ -7,9 +7,31 @@ import polars as pl
 from scipy.spatial import KDTree
 
 from ukroutes.common.logger import logger
-from ukroutes.common.utils import Paths, filter_deadends
+from ukroutes.common.utils import Paths #, filter_deadends
 from ukroutes.process_routing import add_to_graph
 
+def filter_deadends(nodes, edges):
+    G = cugraph.Graph()
+    G.from_cudf_edgelist(
+        edges, source="start_node", destination="end_node", edge_attr="time_weighted"
+    )
+    components = cugraph.connected_components(G)
+    component_counts = components["labels"].value_counts().reset_index()
+    component_counts.columns = ["labels", "count"]
+
+    largest_component_label = component_counts[
+        component_counts["count"] == component_counts["count"].max()
+    ]["labels"][0]
+
+    largest_component_nodes = components[
+        components["labels"] == largest_component_label
+    ]["vertex"]
+    filtered_edges = edges[
+        edges["start_node"].isin(largest_component_nodes)
+        & edges["end_node"].isin(largest_component_nodes)
+    ]
+    filtered_nodes = nodes[nodes["node_id"].isin(largest_component_nodes)]
+    return filtered_nodes, filtered_edges
 
 def process_road_edges() -> pl.DataFrame:
     """
