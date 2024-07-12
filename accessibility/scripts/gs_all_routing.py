57c9f8965ecedd84533c75fc98256e4283b83f82
Mark A Green <mark.green@liverpool.ac.uk>
2024-07-12 13:41:51 +0100

    added more detail to notebook

--- accessibility/scripts/gs_all_routing.py
+++ accessibility/scripts/gs_all_routing.py
@@ -1,3 +1,4 @@
+# Import libraries, packages and functions
 import cudf
 import geopandas as gpd
 import numpy as np
@@ -6,102 +7,71 @@ import cugraph
 import warnings
 
 from ukroutes import Routing
-from ukroutes.common.utils import Paths
+from ukroutes.common.utils import Paths #, filter_deadends
 from ukroutes.preprocessing import process_os
 from ukroutes.process_routing import add_to_graph, add_topk
 
-# To stop warnings being printed, which will lead to a million of them
+# To stop warnings being printed, which will lead to a million of them on Colab
 warnings.filterwarnings("ignore", category=FutureWarning, module="cugraph")
 
-# I have already done this step in R
-#def process_ev():
-#    ev = pd.read_csv(
-#        Paths.RAW / "process" / "national-charge-point-registry.csv",
-#        usecols=["name", "latitude", "longitude"],
-#    )
-#    ev = gpd.GeoDataFrame(
-#        ev,
-#        geometry=gpd.points_from_xy(ev["longitude"], ev["latitude"]),
-#        crs=4326,
-#    ).to_crs(27700)
-#    ev["easting"], ev["northing"] = (ev.geometry.x, ev.geometry.y)
-#    ev = (
-#        ev.drop(columns=["geometry", "latitude", "longitude"])
-#        .replace([np.inf, -np.inf], np.nan)
-#        .dropna(how="any")
-#    )
-#    return ev
+# Load destinations
+# Suggest that you load one at a time, run the script, then re-run the script again with a different one (yes lazy as easier than writing a loop or several scripts)
+greenspace = pd.read_parquet(Paths.PROCESSED / "osgsl" / "osgsl_all.parquet") # All green spaces of any size / type
+#greenspace = pd.read_parquet(Paths.PROCESSED / "osgsl" / "osgsl_doorstop.parquet") # Doorstop greenspace
+#greenspace = pd.read_parquet(Paths.PROCESSED / "osgsl" / "osgsl_local.parquet") # Local greenspace
+#greenspace = pd.read_parquet(Paths.PROCESSED / "osgsl" / "osgsl_neighbourhood.parquet") # Neighbourhood greenspace
+#greenspace = pd.read_parquet(Paths.PROCESSED / "osgsl" / "osgsl_district.parquet") # District greenspace
+#greenspace = pd.read_parquet(Paths.PROCESSED / "osgsl" / "osgsl_wider.parquet") # Wider greenspace
+#greenspace = pd.read_parquet(Paths.PROCESSED / "osgsl" / "osgsl_subregional.parquet") # Sub-regional greenspace
 
-
-# ev_raw = process_ev()
-greenspace = pd.read_parquet(Paths.RAW / "osgsl" / "osgsl.parquet")
-# process_os() # As done the preprocessing previously
-
+# Load road network
 nodes: cudf.DataFrame = cudf.from_pandas(
-    pd.read_parquet(Paths.OS_GRAPH / "nodes.parquet")
+    pd.read_parquet(Paths.OS_GRAPH / "nodes.parquet") # Nodes
 )
 edges: cudf.DataFrame = cudf.from_pandas(
-    pd.read_parquet(Paths.OS_GRAPH / "edges.parquet")
+    pd.read_parquet(Paths.OS_GRAPH / "edges.parquet") # Edges
 )
 
-def filter_deadends(nodes, edges):
-    G = cugraph.Graph()
-    G.from_cudf_edgelist(
-        edges, source="start_node", destination="end_node", edge_attr="time_weighted"
-    )
-    components = cugraph.connected_components(G)
-    component_counts = components["labels"].value_counts().reset_index()
-    component_counts.columns = ["labels", "count"]
-
-    largest_component_label = component_counts[
-        component_counts["count"] == component_counts["count"].max()
-    ]["labels"][0]
-
-    largest_component_nodes = components[
-        components["labels"] == largest_component_label
-    ]["vertex"]
-    filtered_edges = edges[
-        edges["start_node"].isin(largest_component_nodes)
-        & edges["end_node"].isin(largest_component_nodes)
-    ]
-    filtered_nodes = nodes[nodes["node_id"].isin(largest_component_nodes)]
-    return filtered_nodes, filtered_edges
-
-nodes, edges = filter_deadends(nodes, edges)
-
+# Link the greenspace access points to the road network (1 = closest one, but can increase if want to match to more parts of the networks)
 greenspace, nodes, edges = add_to_graph(greenspace, nodes, edges, 1)
 
-postcodes = pd.read_parquet(Paths.PROCESSED / "postcodes.parquet")
-postcodes, nodes, edges = add_to_graph(postcodes, nodes, edges, 2)
-greenspace, postcodes = add_topk(greenspace, postcodes, 3)
-# greenspace = greenspace.dropna()
+# Load households (TOIDs)
+toids = pd.read_parquet(Paths.PROCESSED / "toids_cm_osgb.parquet") # Load
+# toids = toids.sample(100, random_state = 1234) # For testing purposes, subset a smaller dataset
 
+# Link each household to the road network
+toids, nodes, edges = add_to_graph(toids, nodes, edges, 2) # Here we have found that using n=2 is better as sometimes the closest road network piece is not always the best value
+
+# Match green spaces to TOIDs
+greenspace = add_topk(greenspace, toids, 3)
+
+# Define the routing parameters
 routing = Routing(
     name="greenspace",
     edges=edges,
     nodes=nodes,
-    outputs=postcodes,
+    outputs=toids,
     inputs=greenspace,
-    weights="time_weighted",
+    #weights="time_weighted", # Use to get time (mins)
+    weights="length", # Use to get the distance (meters)
     min_buffer=5000,
     max_buffer=500_000,
-    #cutoff=60,
+    #cutoff=60, # Max value - so here 60 for time would be that if route is > 60 mins, then just set value as 60
 )
-routing.fit()
-#distances = routing.fetch_distances()
 
-#distances = (
-#    distances.set_index("vertex")
-#    .join(postcodes.set_index("node_id"), how="right")
-#    .reset_index()
-#)
+# Process the data to estimate the routes between TOIDs and greenspaces
+routing.fit()
+
+# Get distances
 routing.distances
 
+# Store the distances
 distances = (
     routing.distances.set_index("vertex")
-    .join(cudf.from_pandas(postcodes).set_index("node_id"), how="right")
+    .join(cudf.from_pandas(toids).set_index("node_id"), how="right")
     .reset_index()
 )
 
+# Save the output
 OUT_FILE = Paths.OUT_DATA / "distances_greenspace_topk3.csv"
-distances[["postcode", "distance"]].to_csv(OUT_FILE, index=False)
+distances[["TOID", "distance"]].to_csv(OUT_FILE, index=False)
