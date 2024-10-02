7e7bf6037b7e56ca8d84144d7ba19ec1f1add6bb
Roberto Villegas-Diaz <bthillo@hotmail.com>
2024-10-02 14:57:47 +0100

    Add data quality checks for bluespace distance indicator

--- accessibility/access_indicators_dq.R
+++ accessibility/access_indicators_dq.R
@@ -1,13 +1,13 @@
 # 1. Setup ----
 # set location for outputs
 data_dir <- Sys.getenv("GW_RDD") |>
-  file.path("UPRN/distance to nearest green space mag")
+  file.path("UPRN")
 
 # 2. Data Quality assessment ----
 # create temporary file
 tmp_file <- tempfile(fileext = ".zip")
 # download latest OS Open UPRN dataset
-oopt <- options(timeout = 120)
+oopt <- options(timeout = 240)
 download.file(
   url = "https://api.os.uk/downloads/v1/products/OpenUPRN/downloads?area=GB&format=CSV&redirect",
   destfile = tmp_file
@@ -25,7 +25,7 @@ osopenuprn <- files[length(files)] |> # read the latest version
 ## 2.1. greenspace distances ----
 # load the output with the green spaces distances
 greenspace_distances <-
-  file.path(data_dir, "uprn_greenspace_distances.csv") |>
+  file.path(data_dir, "distance to nearest green space mag/uprn_greenspace_distances.csv") |>
   readr::read_csv()
 # tidy up the data
 greenspace_distances_v2 <- greenspace_distances |>
@@ -61,10 +61,48 @@ greenspace_distances_v3 <- greenspace_distances_v2 |>
 ## without coordinates
 greenspace_distances_v3 |>
   dplyr::select(-latitude, -longitude) |>
-  readr::write_excel_csv(file.path(data_dir, "UPRN_2_1_greenspace_distances.csv"))
+  readr::write_excel_csv(file.path(data_dir, "distance to nearest green space mag/UPRN_2_1_greenspace_distances.csv"))
 ## with coordinates
 greenspace_distances_v3 |>
-  readr::write_excel_csv(file.path(data_dir, "UPRN_2_1_greenspace_distances_with_coords.csv"))
+  readr::write_excel_csv(file.path(data_dir, "distance to nearest green space mag/UPRN_2_1_greenspace_distances_with_coords.csv"))
+
+## 2.2. bluespace distances ----
+# load the output with the blue spaces distances
+bluespace_distances <-
+  file.path(data_dir, "Blue space indicators/uprn_bluespace_distances.csv") |>
+  readr::read_csv()
+# tidy up the data
+bluespace_distances_v2 <- bluespace_distances |>
+  dplyr::select(-1) |> # drop rown numbers
+  magrittr::set_names( # update column names
+    c(
+      "TOID",
+      "UPRN",
+      "lad23cd",
+      "distance_any_bluespace"
+    )
+  ) |>
+  dplyr::select(-TOID, -lad23cd) |>
+  dplyr::arrange(UPRN) |>
+  dplyr::distinct() # drop duplicated rows
+
+# drop 'deprecated' UPRNS (i.e., UPRNs not in the OS Open UPRN dataset)
+bluespace_distances_v3 <- bluespace_distances_v2 |>
+  dplyr::left_join(
+    osopenuprn |>
+      dplyr::select(UPRN, latitude = LATITUDE, longitude = LONGITUDE),
+    by = "UPRN"
+  ) |>
+  dplyr::filter(!is.na(latitude), !is.na(longitude))
+
+# store the new version of the data set
+## without coordinates
+bluespace_distances_v3 |>
+  dplyr::select(-latitude, -longitude) |>
+  readr::write_excel_csv(file.path(data_dir, "Blue space indicators/UPRN_4_1_bluespace_distances.csv"))
+## with coordinates
+bluespace_distances_v3 |>
+  readr::write_excel_csv(file.path(data_dir, "Blue space indicators/UPRN_4_1_bluespace_distances_with_coords.csv"))
 
 # remove intermediate files
 unlink(tmp_file)
@@ -72,7 +110,7 @@ unlink(tmp_file)
 # 3. Visualisations ----
 ## 3.1. greenspace distances ----
 # re-load indicator (in case only the visualisations are executed)
-greenspace_distances_v3 <- file.path(data_dir, "UPRN_2_1_greenspace_distances_with_coords.csv") |>
+greenspace_distances_v3 <- file.path(data_dir, "distance to nearest green space mag/UPRN_2_1_greenspace_distances_with_coords.csv") |>
   readr::read_csv()
 
 # create spatial object for the metric
@@ -126,7 +164,7 @@ greenspace_distances_v3_sf |>
   )
 
 ggplot2::ggsave(
-  filename = file.path(data_dir, "UPRN_2_1_greenspace_distances_any_greenspace.png"),
+  filename = file.path(data_dir, "distance to nearest green space mag/UPRN_2_1_greenspace_distances_any_greenspace.png"),
   width = 12,
   height = 11,
   dpi = 300
@@ -158,7 +196,7 @@ greenspace_distances_v3_sf |>
   )
 
 ggplot2::ggsave(
-  filename = file.path(data_dir, "UPRN_2_1_greenspace_distances_doorstop_greenspace.png"),
+  filename = file.path(data_dir, "distance to nearest green space mag/UPRN_2_1_greenspace_distances_doorstop_greenspace.png"),
   width = 12,
   height = 11,
   dpi = 300
@@ -166,6 +204,7 @@ ggplot2::ggsave(
 
 ### distance_local_greenspace ----
 greenspace_distances_v3_sf |>
+  # dplyr::filter(abs(distance_local_greenspace) <= 300) |>
   dplyr::arrange(distance_local_greenspace) |>
   ggplot2::ggplot() +
   ggplot2::geom_sf(ggplot2::aes(fill = distance_local_greenspace),
@@ -190,7 +229,7 @@ greenspace_distances_v3_sf |>
   )
 
 ggplot2::ggsave(
-  filename = file.path(data_dir, "UPRN_2_1_greenspace_distances_local_greenspace.png"),
+  filename = file.path(data_dir, "distance to nearest green space mag/UPRN_2_1_greenspace_distances_local_greenspace.png"),
   width = 12,
   height = 11,
   dpi = 300
@@ -222,7 +261,7 @@ greenspace_distances_v3_sf |>
   )
 
 ggplot2::ggsave(
-  filename = file.path(data_dir, "UPRN_2_1_greenspace_distances_neighbourhood_greenspace.png"),
+  filename = file.path(data_dir, "distance to nearest green space mag/UPRN_2_1_greenspace_distances_neighbourhood_greenspace.png"),
   width = 12,
   height = 11,
   dpi = 300
@@ -254,7 +293,7 @@ greenspace_distances_v3_sf |>
   )
 
 ggplot2::ggsave(
-  filename = file.path(data_dir, "UPRN_2_1_greenspace_distances_wider_greenspace.png"),
+  filename = file.path(data_dir, "distance to nearest green space mag/UPRN_2_1_greenspace_distances_wider_greenspace.png"),
   width = 12,
   height = 11,
   dpi = 300
@@ -286,7 +325,7 @@ greenspace_distances_v3_sf |>
   )
 
 ggplot2::ggsave(
-  filename = file.path(data_dir, "UPRN_2_1_greenspace_distances_district_greenspace.png"),
+  filename = file.path(data_dir, "distance to nearest green space mag/UPRN_2_1_greenspace_distances_district_greenspace.png"),
   width = 12,
   height = 11,
   dpi = 300
@@ -318,9 +357,72 @@ greenspace_distances_v3_sf |>
   )
 
 ggplot2::ggsave(
-  filename = file.path(data_dir, "UPRN_2_1_greenspace_distances_subregional_greenspace.png"),
+  filename = file.path(data_dir, "distance to nearest green space mag/UPRN_2_1_greenspace_distances_subregional_greenspace.png"),
   width = 12,
   height = 11,
   dpi = 300
 )
+
+## 3.2. bluespace distances ----
+# re-load indicator (in case only the visualisations are executed)
+bluespace_distances_v3 <- file.path(data_dir, "Blue space indicators/UPRN_4_1_bluespace_distances_with_coords.csv") |>
+  readr::read_csv()
+
+# create spatial object for the metric
+bluespace_distances_v3_sf <- bluespace_distances_v3 |>
+  sf::st_as_sf(coords = c("longitude", "latitude"), crs = 4326)
+
+# load reference LAs divisions
+### source: https://geoportal.statistics.gov.uk/datasets/34cae3dd946847f78b6dfc6bd5add3c7_0/
+uk_counties <-
+  file.path(Sys.getenv("DATA_DIR"),
+            "Counties_and_Unitary_Authorities_May_2023_UK_BFC.gpkg") |>
+  sf::read_sf() |>
+  janitor::clean_names() |>
+  dplyr::filter(
+    ctyua23nm %in% c(
+      "Cheshire East",
+      "Cheshire West and Chester",
+      "Halton",
+      "Knowsley",
+      "Liverpool",
+      "Sefton",
+      "St. Helens",
+      "Warrington",
+      "Wirral"
+    )
+  )
+
+### distance_any_bluespace ----
+bluespace_distances_v3_sf |>
+  dplyr::arrange(distance_any_bluespace) |>
+  ggplot2::ggplot() +
+  ggplot2::geom_sf(ggplot2::aes(fill = distance_any_bluespace),
+                   stroke = 0.01,
+                   size = 0.75,
+                   shape = 21) +
+  ggplot2::geom_sf(data = uk_counties, fill = NA) +
+  ggplot2::scale_fill_distiller(
+    palette = "Spectral",
+    breaks = scales::breaks_pretty(9)
+  ) +
+  ggplot2::labs(
+    title = "UPRN level bluespace access indicator",
+    subtitle = "Distance to any bluespace",
+    fill = "Distance [m]",
+    x = "Longitude",
+    y = "Latitude"
+  ) +
+  ggplot2::theme_bw() +
+  ggplot2::guides(
+    fill = ggplot2::guide_legend(override.aes = list(size = 3, stroke = .5))
+  )
+
+ggplot2::ggsave(
+  filename = file.path(data_dir, "Blue space indicators/UPRN_4_1_bluespace_distances_any_bluespace.png"),
+  width = 12,
+  height = 11,
+  dpi = 300
+)
+
 
